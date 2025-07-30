400a8f6b00b3f818e0c95887ffd09bc2d5bd2729
unknown <HP@K.ps.int>
2025-07-30 15:06:49 +0200

    gave you your fib

--- nasir.py
+++ nasir.py
@@ -1,1 +1,16 @@
-print("hello Nas edit me to fibonacci")
+print("hello Nas this is your fib")
+def fib(n):
+    a = 0
+    b = 1
+    if n < 1:
+        print("invalid")
+    else:
+        print(a)
+        print(b)
+
+        for i in range(2,n):
+             c = a + b
+             a = b
+             b = c
+             print(c)
+fib(5)
